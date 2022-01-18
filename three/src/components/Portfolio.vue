da84fe60a5a242b0ade77e4eaa5b0124e4750b3a
kashishs1993 <kashish@uni-bremen.de>
2022-01-18 17:36:32 +0100

    commit on vue_from_cli

---
+++ three/src/components/Portfolio.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="portfolio">
+        <p> {{des}} </p>
+        <input v-model="addDescription">
+        <p> {{addDescription}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Portfolio',
+    props: {
+        des: String,
+        addDes: String
+    }
+
+}
+</script>
+
+<style>
+p {
+    color:rgb(0, 0, 0)
+}
+</style>
